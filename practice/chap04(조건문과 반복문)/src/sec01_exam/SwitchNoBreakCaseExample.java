6aa9cccdffd5fbe0672c872f4bb7e2033410ece4
Hyun <hickee32@gmail.com>
2023-03-25 18:56:40 +0900

    conditional, iteration

---
+++ practice/chap04(조건문과 반복문)/src/sec01_exam/SwitchNoBreakCaseExample.java
@@ -0,0 +1,26 @@
+package sec01_exam;
+
+public class SwitchNoBreakCaseExample {
+
+	public static void main(String[] args) {
+
+int time = (int)(Math.random()*4) +8;
+System.out.println("[현재시간: "+ time +"   시]");
+
+switch(time) {
+case 8:
+	System.out.println("출근");
+	break;
+case 9:
+	System.out.println("회의");
+	break;
+case 10:
+	System.out.println("업무");
+	break;
+default:
+	System.out.println("외근");
+	break;
+}
+	}
+
+}
---
+++ practice/chap04(조건문과 반복문)/src/sec02_exam/WhileExample_1.java
@@ -0,0 +1,21 @@
+package sec02_exam;
+
+public class WhileExample_1 {
+
+	public static void main(String[] args) {
+
+		int i =1; 
+		//1~10까지 콘솔에 출력
+		//while문은 보통 무한루프때 많이 사용됨
+		//무한루프를 탈출할 조건문이 반드시 존재해야한다.
+		
+		while (true) {
+			//if (i>=11)
+			//break;
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
