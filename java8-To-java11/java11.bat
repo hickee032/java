9381b73ea8b0d7e8a359fa695b941d6a60eb8917
Hyun <hickee32@gmail.com>
2023-03-25 18:46:16 +0900

    java8 change java11

---
+++ java8-To-java11/java11.bat
@@ -0,0 +1,5 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.8
+set PATH=%JAVA_HOME%\bin;%PATH%
+echo Display Java11 version
+java -version
---
+++ java8-To-java11/java8.bat
@@ -0,0 +1,5 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_261
+set PATH=%JAVA_HOME%\bin;%PATH% 
+echo Display Java8 version
+java -version
